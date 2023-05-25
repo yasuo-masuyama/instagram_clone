adb4a3527c7c090259f0f0e114e75a567393db7b
yasuo masuyama <y.m19980526@gmail.com>
2023-05-25 15:02:21 +0900

    内容修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_params(@user.id)
     else
       render :new
@@ -13,15 +16,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[id])
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
-
-
-
 end
